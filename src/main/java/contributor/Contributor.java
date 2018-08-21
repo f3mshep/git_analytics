b2af3a15492c8943e04504c42e3cfec32b51c2f1
f3mshep <superbiscuit@gmail.com>
2018-08-21 13:23:41 -0700

    added basic models

--- src/main/java/contributor/Contributor.java
+++ src/main/java/contributor/Contributor.java
@@ -1,4 +1,20 @@
 package contributor;
 
+import commit.Commit;
+
+import java.util.Collection;
+
 public class Contributor {
+
+    private Collection<Commit> commits;
+
+    private String name;
+
+    public Contributor(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/repo/Repo.java
+++ src/main/java/repo/Repo.java
@@ -1,4 +1,31 @@
 package repo;
 
+import commit.Commit;
+import contributor.Contributor;
+
+import java.util.Collection;
+
 public class Repo {
+
+    Collection<Contributor> contributors;
+    Collection<Commit> commits;
+    String name, desc, url;
+
+    public Repo(String name, String desc, String url) {
+        this.name = name;
+        this.desc = desc;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
